cf9c28ede873cf31f372bc26ee188e835a1dbf42
Patrick Conheady <patrick.conheady@servian.com>
2021-03-04 16:28:53 +1100

    Script uses curl to wait for favourable response from URL before proceeding

--- wait_for_url.sh
+++ wait_for_url.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
-echo "Running wait script" >&2
+#set -e
 
-echo "$@" >&2
+# Get URL from command-line arguments
+url="$1"
+if [ -z "${url}" ]; then echo >&2 'Error: URL not specified'; exit 1; fi
 
-echo "I think the URL is $1" >&2
+# Other configuration
+wait_secs=5
 
-sleep 2
+echo "Running wait script for URL ${url}" >&2
 
-echo >&2 "Done wait script"
+# Loop until done (one way or the other)
+tries_remaining=3
+while true
+do
+    echo >&2 "Trying ${url}"
+    curl -s -f --max-time "${wait_secs}" "${url}"
+    exit_code="$?"
+    echo >&2 "Did curl, got $exit_code"
+    # Exit code 0 = success, break and move on to victory
+    if [ "${exit_code}" == 0 ]
+    then
+        break
+    fi
+    echo >&2 Was not successful
+    # Exit code 28 = timeout, decrement remaining retries and loop again
+    # So if it's not 28 then explode in flames.
+    if [ "${exit_code}" != 28 ]
+    then
+        echo >&2 "Unexpected error ${exit_code} from curl"
+        exit "${exit_code}"
+    fi
+    # Decrement and loop if we have retries remaining
+    ((tries_remaining -= 1))
+    if [ "${tries_remaining}" -le 0 ]
+    then
+        echo >&2 "Exhausted retries, exiting"
+        exit 1
+    fi
+done
 
+# Send state back to Terraform for successful completion
 echo '{"url": "'"$1"'"}'
